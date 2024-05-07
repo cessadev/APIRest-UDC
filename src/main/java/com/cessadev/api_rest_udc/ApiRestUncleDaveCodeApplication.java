3d582226765d975966355458c746ad94997d8242
César Urbiña <cesarurb.dev@gmail.com>
2024-05-07 11:18:31 -0500

    Initial commit

---
+++ src/main/java/com/cessadev/api_rest_udc/ApiRestUncleDaveCodeApplication.java
@@ -0,0 +1,13 @@
+package com.cessadev.api_rest_udc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestUncleDaveCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestUncleDaveCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cessadev/api_rest_udc/ApiRestUncleDaveCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cessadev.api_rest_udc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestUncleDaveCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

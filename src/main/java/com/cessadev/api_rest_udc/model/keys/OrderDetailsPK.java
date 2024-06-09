203649d2ef6b7432299c3b12bf0544e7807beb68
cessadev <cesarurb.dev@gmail.com>
2024-06-09 14:53:29 -0500

    Othes settings

--- src/main/java/com/cessadev/api_rest_udc/model/keys/OrderDetailsPK.java
+++ src/main/java/com/cessadev/api_rest_udc/model/keys/OrderDetailsPK.java
@@ -11,4 +11,22 @@ public class OrderDetailsPK implements Serializable {
 
     private Order order;
     private Product product;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderDetailsPK that = (OrderDetailsPK) o;
+
+        if (!order.equals(that.order)) return false;
+        return product.equals(that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = order.hashCode();
+        result = 31 * result + product.hashCode();
+        return result;
+    }
 }
